acad931b55b8eae6dcae853514bfef51cf9ae50f
AlliceSpring <allicespring21@gmail.com>
2016-09-18 16:57:36 +0200

    add comment

--- src/main/java/pl/michalczukm/isa/java/App.java
+++ src/main/java/pl/michalczukm/isa/java/App.java
@@ -7,9 +7,7 @@ public class App
 {
     public static void main( String[] args ) {
 
-        PatientService patientService = new PatientService();
-        List<Patient> patients = patientService.getAll();
-
+//here iterate over patient
         for (Patient patient : patients) {
             System.out.println(String.format("%1$s", patient.getFirstName()));
             System.out.println(String.format("%1$s", patient.getLastName()));
