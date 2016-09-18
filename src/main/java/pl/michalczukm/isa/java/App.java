3f20be0e63e716fb74ecfa1d8ec88deccbfa759b
AlliceSpring <allicespring21@gmail.com>
2016-09-18 17:14:02 +0200

    ten commit jest probny i nie na serio

--- src/main/java/pl/michalczukm/isa/java/App.java
+++ src/main/java/pl/michalczukm/isa/java/App.java
@@ -6,8 +6,6 @@ import java.util.List;
 public class App 
 {
     public static void main( String[] args ) {
-
-//here iterate over patient
         for (Patient patient : patients) {
             System.out.println(String.format("%1$s", patient.getFirstName()));
             System.out.println(String.format("%1$s", patient.getLastName()));
---
+++ src/main/java/pl/michalczukm/isa/java/Wizyta.java
@@ -0,0 +1,7 @@
+package pl.michalczukm.isa.java;
+
+/**
+ * Created by ablazejewska on 18.09.16.
+ */
+public class Wizyta {
+}
