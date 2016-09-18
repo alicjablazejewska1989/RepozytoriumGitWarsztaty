6b2c1b4bd4add9334fe63d218b10a28c658d3e35
AlliceSpring <allicespring21@gmail.com>
2016-09-18 16:14:01 +0200

    add doktor (patient)

---
+++ src/main/java/pl/michalczukm/isa/java/DoctorSevice.java
@@ -0,0 +1,10 @@
+package pl.michalczukm.isa.java;
+
+/**
+ * Created by ablazejewska on 18.09.16.
+ */
+public class DoctorSevice {
+    public DoctorSevice(){
+        new Patient("Adam","Bania",22);
+    }
+}
